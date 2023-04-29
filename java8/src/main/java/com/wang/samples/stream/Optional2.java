b84beef98d2f7725fd9fb035f09a359f0e827735
wangshenzhen0718 <57970628+wangshenzhen0718@users.noreply.github.com>
2023-04-29 19:12:11 +0800

    map避免空指针异常

---
+++ java8/src/main/java/com/wang/samples/stream/Optional2.java
@@ -0,0 +1,79 @@
+package com.wang.samples.stream;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+/**
+ * @Author:wsz
+ * @Date: 2023/4/29 18:39
+ * @Description: map避免空指针异常
+ * @Version: 1.0
+ * @Since: 1.0
+ */
+public class Optional2 {
+    private static void test1() {
+        Optional.of(new Outer())
+                .flatMap(o -> Optional.ofNullable(o.nested))
+                .flatMap(n -> Optional.ofNullable(n.inner))
+                .flatMap(i -> Optional.ofNullable(i.foo))
+                .ifPresent(System.out::println);
+    }
+
+
+    private static void test2() {
+        Optional.of(new Outer())
+                .map(Outer::getNested)
+                .map(Nested::getInner)
+                .map(Inner::getFoo)
+                .ifPresent(System.out::println);
+    }
+
+    private static void test3() {
+        Outer outer = new Outer();
+        resolve(() -> outer.getNested().getInner().getFoo())
+                .ifPresent(System.out::println);
+    }
+
+
+
+
+    public static void main(String[] args){
+        //test1();
+        //test2();
+        test3();
+    }
+
+    public static <T> Optional<T> resolve(Supplier<T> resolver) {
+        try {
+            T result = resolver.get();
+            return Optional.ofNullable(result);
+        }
+        catch (NullPointerException e) {
+            return Optional.empty();
+        }
+    }
+
+    static class Inner {
+        String foo = "boo";
+
+        public String getFoo() {
+            return foo;
+        }
+    }
+
+    static class Nested {
+        Inner inner = new Inner();
+
+        public Inner getInner() {
+            return inner;
+        }
+    }
+
+    static class Outer {
+        Nested nested = new Nested();
+
+        public Nested getNested() {
+            return nested;
+        }
+    }
+}
